09d5bef8646c324cb9312858fa2851933118baac
itsLohit <109834876+itsLohit@users.noreply.github.com>
2025-05-14 07:15:24 +0530

    Completed fibonacci sequence in iterative method

---
+++ fibonacci/fibs.js
@@ -0,0 +1,16 @@
+const fibs = (num) => {
+    let array = [0];
+    let prev2 = 0;
+    let prev1 = 1;
+    
+    for(let i=1; i<num; i++){
+        let nextNum = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = nextNum;
+        array.push(nextNum);
+    }
+
+    return array;
+}
+
+console.log(fibs(8));
